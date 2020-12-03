d9466aeaf17cf6ee93391a0e123bafc3ec062daf
categoryK <categoryK@DESKTOP-8Q6NURQ>
2020-12-03 17:43:13 +0900

    8교시 마무리 for반복문 공부

---
+++ src/test/java/kr/or/test/HelloWorldApp.java
@@ -0,0 +1,10 @@
+package kr.or.test;
+
+public class HelloWorldApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 위 main()메서드 자바프로그램 실행 진입지점입니다.
+		System.out.println("헬로우 월드!");
+	}
+
+}
