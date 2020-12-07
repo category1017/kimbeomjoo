00e380747e39c04d223d193d8feebd284a082fbd
categoryK <categoryK@DESKTOP-8Q6NURQ>
2020-12-07 15:32:50 +0900

    6교시 커밋

--- src/test/java/kr/or/test/ExceptionTest.java
+++ src/test/java/kr/or/test/ExceptionTest.java
@@ -3,12 +3,24 @@ package kr.or.test;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.edu.vo.MemberVO;
+
 /**
  * 자바에서 예외처리하는 테스트 클래스
  * */
 public class ExceptionTest {
 
 	public static void main(String[] args) {
+		//toString메서드에 대해서...
+		MemberVO memberVO = new MemberVO();//jsp폼데이터를 set으로 저장, get으로 출력.
+		//멤버클래스에 값을 입력해 봅니다. 메모리DB에 입력함
+		memberVO.setUser_id("beomjoo");
+		memberVO.setUser_name("김범주");
+		memberVO.setEmail("abc@abc.com");
+		System.out.println("회원정보 클래스의 멤버변수 내용 값들을 출력하기 = " 
+		+ memberVO.getUser_name()
+		+ memberVO.toString()
+		);
 		// 집입실행메서드 main()
 		// 메서드 내부변수 (멤버 변수는 class바로 아래에 있어야함.)
 		String[] stringArray = {"10", "2a", "100"};//2a : 강제로 예외사항 발생하기 위해 
