0fbf86a891bd07690ecafb43a22af5138a78400b
categoryK <categoryK@DESKTOP-8Q6NURQ>
2020-12-17 17:52:55 +0900

    8교시 마무리 과제제출포함

--- src/test/java/kr/or/test/OpenApi.java
+++ src/test/java/kr/or/test/OpenApi.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Calendar;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * OpenApi클래스로 HRD-Net에서 제공하는 구직자훈련과정API 목록을 출력하는 자바앱
@@ -16,7 +19,7 @@ public class OpenApi {
 	//외부연계 역할 메서드(아래)
 	public static void serviceApi() {
 		BufferedReader bufferedReader = null; //HRD넷에서 전송받은 데이터를 임시 저장하는 공간.자동차 범퍼와 같은 역할.
-		String urlStr = "http://www.hrd.go.kr/hrdp/api/apipo/APIPO0101T.do?authKey=EZHD1wsQ13TSpm1YxE4hgqamrpfhScQu&outType=1&srchTraEndDt=20210317&srchTraArea1=44&srchTraArea2=44133&srchTraStDt=20201108&srchTraOrganNm=%ED%9C%B4%EB%A8%BC&sortCol=TR_STT_DT&returnType=XML&pageSize=10&pageNum=1&sort=ASC&srchTraPattern=N1&srchPart=-99&apiRequstPageUrlAdres=/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_1.jsp&apiRequstIp=123.215.61.179";
+		String urlStr = "http://www.hrd.go.kr/hrdp/api/apipo/APIPO0101T.do?returnType=XML&pageSize=10&srchTraArea1=44&authKey=EZHD1wsQ13TSpm1YxE4hgqamrpfhScQu&sort=DESC&outType=1&srchTraStDt=20201101&pageNum=1&sortCol=TR_STT_DT&srchTraEndDt=20201231&srchTraPattern=N1&srchPart=-99&apiRequstPageUrlAdres=/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_1.jsp&apiRequstIp=123.215.61.179";
 		try {
 			URL url = new URL(urlStr);
 			try {
@@ -29,7 +32,7 @@ public class OpenApi {
 				//while 반복문시작
 				while( (line=bufferedReader.readLine()) !=null) {
 					cnt = cnt + 1;
-					result = result + line + "\n" ; // \n은 newline나타내는 기호로 결과는 엔터
+					result = result + line + "\n" ; // \n은 newline나타내는 기호로 결과는 엔터이다.
 				}
 				System.out.println("버퍼리더로 읽어들인 최종 결과값은 아래 입니다. while 반복 횟수는 " + cnt);
 				//System.out.println(result);2줄로 읽어서 끝
@@ -44,11 +47,26 @@ public class OpenApi {
 		} catch (MalformedURLException e) {
 			// 외부연계 URL 주소형식이 잘못되었을때 에러상황발생
 			System.out.println("URL주소형식이 잘못되었습니다.왜냐하면 " + e.toString());
+		}finally {
+			//콘솔화면에 현재 PC시간을 표시해서 진짜 5초단위로 스케줄대로 작동되는지 확인
+			Calendar calendar = Calendar.getInstance();
+			System.out.println(calendar.getTime());
 		}
 	}
 	//스테틱 메서드는 new키워드로 객체오브젝트 생성없이 바로 접근이 가능한 메서드를 말함.(한번만 사용)
 	public static void main(String[] args) {
-		serviceApi();
+		//메인스레드는 1개 다른스레드를 추가로 실행할 때, Runnable메서드를 사용합니다.(아래)
+		//추가스레드를  스케줄로 실행할때(반복해서 주기적으로 실행) 실행간격 변수(5초)
+		int sleepSec = 10;
+		//주기적인 스레드 작업(Concurrent동시작업)을 위한 코딩 : new키워드로 실행가능한 오브젝트 변수인  exec변수 생성.
+		//final 인 현재클래스에서만 사용하겠다는 명시적인 의미
+		final ScheduledThreadPoolExecutor exec = new ScheduledThreadPoolExecutor(1);
+		exec.scheduleAtFixedRate(new Runnable() {
+			public void run() {//스레드를 이용해서 5초단위로 메소드를 반복실행
+				serviceApi();
+			}
+		}, 0, sleepSec, TimeUnit.SECONDS);
+		
 		// 일반메서드와 스테틱메서드의 호출차이
 		//StaticTest staticTest = new StaticTest();
 		//staticTest.test2();// 노스태틱은 오브젝트생성후 접근이 가능
