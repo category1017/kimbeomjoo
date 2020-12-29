e2151ae7a6f265429d72423e90afd3467bc957b5
categoryK <categoryK@DESKTOP-8Q6NURQ>
2020-12-29 17:51:25 +0900

    마무리8교시

--- src/test/java/kr/or/test/HelloWorldApp.java
+++ src/test/java/kr/or/test/HelloWorldApp.java
@@ -3,8 +3,14 @@ package kr.or.test;
 public class HelloWorldApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 위 main()메서드 자바프로그램 실행 진입지점입니다.
+		// 위 main()메서드 자바프로그램 실행 진입지점입니다.
 		System.out.println("헬로우 월드!");
+		plusFuncABC(3,4); //플러스 함수를 실행할때 매개변수입니다. 
 	}
 
+	//메서드의 매개변수에 대해서
+	public static void plusFuncABC(int a, int b){
+		System.out.println("매개변수 a + b " + (a+b) + "입니다.");
+	}
+	
 }
